bd1e7eb59756fb5c0e74bdc32ac03a8732d1de91
BIAS04 <mayanksinghparihar554@gmail.com>
2025-11-05 06:49:32 +0530

    11-04-25 Mayank singh parihar fix The Alternating Capatalization

--- Daily-Solutions/11-04-2025/sol.py
+++ Daily-Solutions/11-04-2025/sol.py
@@ -3,4 +3,13 @@ String = input('Enter a string: ')
 String = String.replace('_', '~').replace('-', '~')
 String = String.capitalize()
 
-print(String)
+Output = []
+upper = True
+for char in String:
+    if char.isalpha():
+        Output.append(char.upper() if upper else char.lower())
+        upper = not upper
+    else:
+        Output.append(char)
+
+print("".join(Output))
